e5b9f91a3c554d079cba4de7252e6bdd9cfd3180
cj784005 <118866787+cj784005@users.noreply.github.com>
2022-11-23 10:06:40 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.showNumber(3)
+basic.showNumber(2)
+basic.showNumber(1)
+basic.forever(function () {
+    basic.showIcon(IconNames.Duck)
+    basic.showIcon(IconNames.Tortoise)
+    basic.showIcon(IconNames.Rabbit)
+    basic.showString("SHW")
+})
